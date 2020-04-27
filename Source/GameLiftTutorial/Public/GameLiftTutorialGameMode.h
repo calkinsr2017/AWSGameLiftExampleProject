de7f56ad3fa8186014763c06af16bee832fbd808
Robert <rcalkins@umd.edu>
2020-04-27 11:45:38 -0400

    copied things over

---
+++ Source/GameLiftTutorial/Public/GameLiftTutorialGameMode.h
@@ -0,0 +1,89 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameLiftServerSDK.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameLiftTutorialGameMode.generated.h"
+
+
+USTRUCT()
+struct FStartGameSessionState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	FStartGameSessionState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FUpdateGameSessionState
+{
+	GENERATED_BODY();
+
+	FUpdateGameSessionState() {
+
+	}
+};
+
+USTRUCT()
+struct FProcessTerminateState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	long TerminationTime;
+
+	FProcessTerminateState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FHealthCheckState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	FHealthCheckState() {
+		Status = false;
+	}
+};
+
+UCLASS(minimalapi)
+class AGameLiftTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameLiftTutorialGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+		FStartGameSessionState StartGameSessionState;
+	UPROPERTY()
+		FUpdateGameSessionState UpdateGameSessionState;
+
+	UPROPERTY()
+		FProcessTerminateState ProcessTerminateState;
+
+	UPROPERTY()
+		FHealthCheckState HealthCheckState;
+
+
+};
+
+
+
