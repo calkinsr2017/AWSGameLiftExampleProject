f8addee9b24ec9c9cb88913ed95d9d98875ab909
Robert <rcalkins@umd.edu>
2020-06-08 12:02:26 -0400

    Started creating main menu code

---
+++ Source/GameLiftTutorial/Private/MainMenuGameMode.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenuGameMode.h"
+#include "MainMenuHud.h"
+
+AMainMenuGameMode::AMainMenuGameMode()
+{
+	HUDClass = AMainMenuHud::StaticClass();
+}
---
+++ Source/GameLiftTutorial/Private/MainMenuHud.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenuHud.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMainMenuHud::AMainMenuHud()
+{
+	static ConstructorHelpers::FClassFinder<UUserWidget> MainMenuObj(TEXT("/Game/UI/Widgets/UI_MainMenu"));
+	MainMenuWidgetClass = MainMenuObj.Class;
+}
+
+void AMainMenuHud::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if(PlayerController != nullptr)
+	{
+		PlayerController -> bShowMouseCursor = true;
+	}
+
+	if(MainMenuWidgetClass != nullptr)
+	{
+		UUserWidget* MainMenuWidget = CreateWidget<UUserWidget>(GetWorld(), MainMenuWidgetClass);
+		if(MainMenuWidget != nullptr)
+		{
+			MainMenuWidget -> AddToViewport();
+			MainMenuWidget->SetFocus();
+		}
+	}
+}
+
+
