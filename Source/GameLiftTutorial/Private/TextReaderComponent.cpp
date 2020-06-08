39eb69edc810b6f5438d37ea89065727704087f1
MikhailSorokin <sorokinmike96@gmail.com>
2020-06-08 18:01:48 -0400

    Fixed compile error with MainMenuWidget + added in text reader component. Added in a UI for when we get to choosing the different matches.

--- Source/GameLiftTutorial/Private/TextReaderComponent.cpp
+++ Source/GameLiftTutorial/Private/TextReaderComponent.cpp
@@ -3,32 +3,42 @@
 
 #include "TextReaderComponent.h"
 
+
+#include "HAL/FileManager.h"
+#include "Misc/FileHelper.h"
+#include "Runtime/Core/Public/Misc/Paths.h"
+#include "Runtime/Core/Public/HAL/PlatformFilemanager.h"
+
 // Sets default values for this component's properties
 UTextReaderComponent::UTextReaderComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
-void UTextReaderComponent::BeginPlay()
+FString UTextReaderComponent::ReadFile(FString filePath)
 {
-	Super::BeginPlay();
+	//Read file from [project]/filePath/
+	FString FolderPath = FPaths::ProjectContentDir() + "Credentials";
+	FString Directory = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*FolderPath);
 
-	// ...
-	
+	FString Result = "";
+	IPlatformFile& file = FPlatformFileManager::Get().GetPlatformFile();
+	if (file.CreateDirectory(*Directory)) {
+		FString myFile = Directory + "/" + filePath;
+
+		if (GEngine) {
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "Attempting to locate: " + myFile);
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "in directory: " + Directory);
+		}
+
+		FFileHelper::LoadFileToString(Result, *myFile);
+
+		if (GEngine) {
+			GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "Result: " + Result);
+		}
+	}
+
+	return Result;
 }
-
-
-// Called every frame
-void UTextReaderComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
--- Source/GameLiftTutorial/Public/TextReaderComponent.h
+++ Source/GameLiftTutorial/Public/TextReaderComponent.h
@@ -16,13 +16,5 @@ public:
 	// Sets default values for this component's properties
 	UTextReaderComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+	FString ReadFile(FString filePath);
 };
