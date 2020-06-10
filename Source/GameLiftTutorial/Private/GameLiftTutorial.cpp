69593d2feadba55bd09b847f79f9ef6b73f79ad3
Abrar <afs167@gmail.com>
2020-06-10 11:47:07 -0400

    Built all the GameMode functionality - largely taken from Chris Gong's project with some refactoring added in.

--- Source/GameLiftTutorial/Private/GameLiftTutorial.cpp
+++ Source/GameLiftTutorial/Private/GameLiftTutorial.cpp
@@ -4,4 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameLiftTutorial, "GameLiftTutorial" );
- 
+
+DEFINE_LOG_CATEGORY(LogAWS)
--- Source/GameLiftTutorial/Public/GameLiftTutorialGameMode.h
+++ Source/GameLiftTutorial/Public/GameLiftTutorialGameMode.h
@@ -2,8 +2,17 @@
 
 #include "CoreMinimal.h"
 #include "GameLiftServerSDK.h"
+#include "HttpModule.h"
 #include "GameFramework/GameModeBase.h"
 #include "GameLiftTutorialGameMode.generated.h"
+
+UENUM()
+enum class EUpdateReason : uint8 {
+	MATCHMAKING_DATA_UPDATED = 0,
+	BACKFILL_FAILED,
+	BACKFILL_TIMED_OUT,
+	BACKFILL_CANCELLED
+};
 
 USTRUCT()
 struct FStartGameSessionState
@@ -18,6 +27,7 @@ struct FStartGameSessionState
 	}
 };
 
+/* Will enable us to get all players in the match */
 USTRUCT()
 struct FUpdateGameSessionState
 {
@@ -26,6 +36,12 @@ struct FUpdateGameSessionState
 	FUpdateGameSessionState() {
 
 	}
+
+	//What is the current state in the game session, actively updated on server aws tick
+	UPROPERTY()
+		EUpdateReason Reason;
+	//Will be a running list of all players actively looking for matchmaking
+		TMap<FString, FPlayer> PlayerIdToPlayer;
 };
 
 USTRUCT()
@@ -64,6 +80,16 @@ class AGameLiftTutorialGameMode : public AGameModeBase
 public:
 	AGameLiftTutorialGameMode();
 
+	//Called when the player logs into the map
+	void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
+	              FString& ErrorMessage) override;
+
+	//Called when player leaves the map
+	void Logout(AController* ExitingPC) override;
+	FString InitNewPlayer(APlayerController* NewPlayerController, const FUniqueNetIdRepl& UniqueId,
+	                      const FString& Options,
+	                      const FString& Portal) override;
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -79,4 +105,7 @@ private:
 
 	UPROPERTY()
 		FHealthCheckState HealthCheckState;
+
+	
+	FHttpModule* HttpModule;
 };
